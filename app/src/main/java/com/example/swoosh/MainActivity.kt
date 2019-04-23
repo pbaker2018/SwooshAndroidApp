404bdf5a5ced74483cdb33deac94fac5cfe7e42e
Pauline <pbaker@hotmail.co.uk>
2019-04-23 17:26:01 +0100

    added 'context and intent' to Main Activity so that pressing button will take user to next layout.

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+            /*the above is an example of explicit intent*/
+            /*it is explicit because we specify the target class*/
+            /*'this' is referring to the context of the activity we are in*/
+        }
+
+
     }
 }
