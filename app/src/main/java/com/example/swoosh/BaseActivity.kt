df8c693a33b5ea4017e428f93e5592f7a4f07eec
Pauline <pbaker@hotmail.co.uk>
2019-04-24 15:03:54 +0100

    added a new layout for page 3, and added code to move from from page 2 to page 3

---
+++ app/src/main/java/com/example/swoosh/BaseActivity.kt
@@ -0,0 +1,51 @@
+package com.example.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+/*This file is being created to log the method of each of the lifecycles when it occurs so we can see, in real time,
+the order in which it is called and how it relates to the current activities we have.*/
+/*We will log a message for EACH of the lifecycle methods*/
+/*'Log' is an API for sending log output*/
+/*when the MainActivity is launched, the Logcat will log the message "MainActivity onCreate"*/
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "${javaClass.simpleName} onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "${javaClass.simpleName} onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} onResume")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} onDestroy")
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun nextClicked(view: View) {
+        val leagueIntent = Intent(this, LevelActivity::class.java)
+        startActivity(leagueIntent)
+    }
+
+    /*the function nextClicked takes place outside of onCreate because I used the onclick section of the buttons attributes.*/
 }
